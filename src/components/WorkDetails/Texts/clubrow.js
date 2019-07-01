4dcd1b79cb9c0537357d9e002143ab633815413b
juji <jujiyangasli@gmail.com>
2019-07-01 23:14:53 +0700

    edit clubrow

--- src/components/WorkDetails/Texts/clubrow.js
+++ src/components/WorkDetails/Texts/clubrow.js
@@ -11,7 +11,7 @@ export default props => {
       I was working with <a href="https://cnd.digital/" target="_blank" rel="noopener noreferrer">CND</a>. We had Clubrow as a client.
       Before we move forward with the proposal, We need to make sure we can read the output of the rowing machine: <a target="_blank" rel="noopener noreferrer" href="https://www.concept2.com/">Concept2</a>.
     </p>
-    <p>So we did... It was an entertaining exercise, doing bit shift operations, like converting RBG to decimal.</p>
+    <p>So we did... It was an entertaining exercise, doing bit shift operations, just like converting RBG to decimal.</p>
     <p>
       It was done using <a target="_blank" rel="noopener noreferrer" href="https://github.com/noble/noble">noble</a>, with infomation in {' '}
       <a target="_blank" rel="noopener noreferrer" href="https://www.concept2.co.uk/files/pdf/us/monitors/PM5_BluetoothSmartInterfaceDefinition.pdf">this doc</a>.
